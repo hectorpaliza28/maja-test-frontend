224dc8d008ffe05f16475748171727c9ec9483be
hectorpaliza28 <86799919+hectorpaliza28@users.noreply.github.com>
2025-01-11 02:25:13 -0700

    se configuran archivos para despliegue en heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const folder = path.join(__dirname, 'dist/maja-test-frontend');
+
+app.use(express.static(folder));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(folder, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
